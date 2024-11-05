24d05de44944c77b04a6bc6a7d9ca422899a08e9
jessekerch <jesse.kercheval@gmail.com>
2024-11-05 08:18:24 -0800

    fix: add clientConfig in config folder, remove setupCredentials from services folder
    Co-authored-by: Joel Barton <joelbarton-io@users.noreply.github.com>
    Co-authored-by: Jesse Kercheval <jessekerch@users.noreply.github.com>
    Co-authored-by: Shane Ziegler <shaneziegler@users.noreply.github.com>
    Co-authored-by: Wook Kim <wook2000@users.noreply.github.com>

--- src/config/clientConfig.ts
+++ src/config/clientConfig.ts
@@ -1,7 +1,6 @@
 /* 
     - creating a basic `config` object that is used throughout the app by all aws-clients
-    - create unique harrier tag that follows a naming convention: 'harrier-<XXXXXXXX>-resource-name'
-    export const config = { user config }
+    export const config = { credentials: }
   */
 
 import "dotenv/config";
