342a146934f2526b4b232748eff63792a52b2e4f
Joel Barton <joelbarton406@gmail.com>
2024-11-05 09:49:45 -0700

    chore: extract into separate files Co-authored-by: Joel Barton <joelbarton-io@users.noreply.github.com> Co-authored-by: Jesse Kercheval <jessekerch@users.noreply.github.com> Co-authored-by: Shane Ziegler <shaneziegler@users.noreply.github.com> Co-authored-by: Wook Kim <wook2000@users.noreply.github.com>

---
+++ src/utils/aws/api/setupRestApi.ts
@@ -0,0 +1,21 @@
+import createRestApi from "./createRestApi";
+import createResource from "./createResource";
+import createMethod from "./createMethod";
+
+export default async function setupRestApi() {
+  try {
+    const restApiId = await createRestApi();
+    const resourceId = await createResource(restApiId);
+    await createMethod(restApiId, resourceId);
+
+    console.log("'done'");
+
+    // create integrations
+    // create resource policy
+    // register api with api gateway
+    // create stages
+    // create deployment
+  } catch (error: unknown) {
+    console.error("Error executing setupRestApi: ", error);
+  }
+}
