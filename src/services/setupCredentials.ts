02b80a016c6c04695964ba87d4c758b68cb51ccb
jessekerch <jesse.kercheval@gmail.com>
2024-11-04 20:56:31 -0800

    feat: setup credentials and return config object
    Co-authored-by: Joel Barton <joelbarton-io@users.noreply.github.com>
    Co-authored-by: Jesse Kercheval <jessekerch@users.noreply.github.com>
    Co-authored-by: Shane Ziegler <shaneziegler@users.noreply.github.com>
    Co-authored-by: Wook Kim <wook2000@users.noreply.github.com>

--- src/services/setupCredentials.ts
+++ src/services/setupCredentials.ts
@@ -2,5 +2,13 @@
     - creating a basic `config` object that is used throughout the app by all aws-clients
     - create unique harrier tag that follows a naming convention: 'harrier-<XXXXXXXX>-resource-name'
     export const config = { user config }
-    */
-export const setupCredentials = () => {};
+  */
+
+import "dotenv/config";
+
+import { fromEnv } from "@aws-sdk/credential-providers";
+
+export const config = {
+  credentials: fromEnv(), // Load credentials from environment variables
+  region: "us-east-1",
+};
