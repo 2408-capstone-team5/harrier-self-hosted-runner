6810cba9161387722695eba61758d5a59dfcf4f1
Wook Kim <h.wook.kim@gmail.com>
2024-11-06 01:18:29 -0800

    chore: finish defining createEC2 util (pending TS resolution)
    Co-authored-by: Jesse Kercheval <jessekerch@users.noreply.github.com>
    Co-authored-by: Joel Barton <joelbarton-io@users.noreply.github.com>
    Co-authored-by: Shane Ziegler <shaneziegler@users.noreply.github.com>
    Co-authored-by: Wook Kim <wook2000@users.noreply.github.com>

--- src/utils/aws/ec2/createEC2.ts
+++ src/utils/aws/ec2/createEC2.ts
@@ -1,48 +1,28 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import {
   EC2Client,
   RunInstancesCommand,
   RunInstancesCommandInput,
-  // _InstanceType,
-  // IamInstanceProfileSpecification,
   TagSpecification,
 } from "@aws-sdk/client-ec2";
 import { config } from "../../../config/client";
-// import { configHarrierType } from "../../../types/typesConfig";
 import { configHarrier } from "../../../config/configHarrier";
-// import { validInstanceTypes } from "../../../types/ec2InstancesType";
 import { getStartScript } from "../../../scripts/setup";
 
-// import { fromIni } from "@aws-sdk/credential-provider-ini"; // For loading credentials from ~/.aws/credentials
-
-// const REGION = "us-east-1"; // Change to your desired region
-
-// // Initialize the EC2 client
-// const client = new EC2Client({
-//   region: REGION,
-//   credentials: fromIni({ profile: "default" }), // Load credentials from the default profile
-// });
-
 export const createEC2 = async () => {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
   const client = new EC2Client(config);
-  const url = "https://github.com/2408-capstone-team5";
-
-  // const instanceType = "t2.micro";
-  // use configHarrier to assign config values to RunInstancesCommand params
-
-  // function isConfigHarrier(obj: configHarrierType): obj is configHarrierType {
-  //   return obj && typeof obj.tagValue === 'string' && typeof obj.cidrBlockVPC === 'string' && typeof obj.cidrBlockSubnet === 'string' && obj.vpcId === 'string' && obj.subnetId === 'string' && obj.region === 'string' && obj.awsAccountId === 'string' && obj.imageId === 'string' && obj.instanceType === 'string' && obj.keyName === 'string' && obj.minInstanceCount === 'number' && obj.maxInstanceCount === 'number' && obj.IamInstanceProfile === 'object';
-  // };
+  const githubUrl = "https://github.com/2408-capstone-team5";
 
   const amiId = configHarrier.imageId;
-  const instanceType = "t2.micro";
-  const keyName = "test-1-ubuntu-64x86-241022";
-  const minCount = 1;
-  const maxCount = 1;
-  const securityGroupIds = ["sg-0f690732e685b371b"];
-  const subnetId = "subnet-0a0ad1280d9d29dd0";
-  const iamInstanceProfile = { Name: "EC2-access-S3" };
+  const instanceType =
+    configHarrier.instanceType === "m7a.medium"
+      ? configHarrier.instanceType
+      : "t2.micro";
+  const keyName = configHarrier.keyName;
+  const minCount = configHarrier.minInstanceCount;
+  const maxCount = configHarrier.maxInstanceCount;
+  const securityGroupIds = ["sg-0f690732e685b371b"]; // We need security groups in configHarrier
+  const subnetId = configHarrier.subnetId;
+  const iamInstanceProfile = configHarrier.IamInstanceProfile;
   const tagSpecifications: TagSpecification[] = [
     {
       ResourceType: "instance",
@@ -51,7 +31,7 @@ export const createEC2 = async () => {
   ];
 
   // const userDataScript = startScript;
-  const userDataScript = getStartScript(url);
+  const userDataScript = getStartScript(githubUrl);
   console.log(userDataScript);
 
   // Encode the script in base64 as required by AWS
