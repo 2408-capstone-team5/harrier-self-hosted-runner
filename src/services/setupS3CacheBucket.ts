4db3939f2a446d1ec820d38b062c770c7f00fc76
jessekerch <jesse.kercheval@gmail.com>
2024-11-06 16:52:18 -0800

    fix: setupCacheEviction without invoke and setupS3Cache
    Co-authored-by: Joel Barton <joelbarton-io@users.noreply.github.com>
    Co-authored-by: Jesse Kercheval <jessekerch@users.noreply.github.com>
    Co-authored-by: Shane Ziegler <shaneziegler@users.noreply.github.com>
    Co-authored-by: Wook Kim <wook2000@users.noreply.github.com>

--- src/services/setupS3CacheBucket.ts
+++ src/services/setupS3CacheBucket.ts
@@ -1,39 +1,16 @@
 /* 
-    - conditionally create and config S3 bucket
-  */
+- conditionally create and config S3 bucket
+*/
 
-import createAndDeployLambda from "../utils/aws/lambda/createAndDeployLambda";
-import getLambdaArn from "../utils/aws/lambda/getLambdaArn";
+import { config } from "../config/client";
+import { createS3 } from "../utils/aws/s3/createS3";
+import { installationHash } from "../config/installationHash";
+import { S3Client } from "@aws-sdk/client-s3";
 
-import createSchedule from "../utils/aws/eventbridge/createSchedule";
+const client = new S3Client(config);
+const maxWaitTime = 60;
 
-export async function setupS3CacheBucket() {
-  const lambdaName = "cache_test_lambda";
-  const lambdaRole = "s3CacheCleanupLambda-role-zp58dx91";
-  const scheduleName = "test-schedule";
-  const scheduleRole = "Amazon_EventBridge_Scheduler_LAMBDA_da0ae2eeec";
-
-  try {
-    await createAndDeployLambda(lambdaName, lambdaRole);
-    // TODO: lambda is using an existing role, need to make one programatically?
-    console.log("lambda created with role to access s3, and deployed");
-
-    const lambdaArn = await getLambdaArn(lambdaName);
-
-    // TODO: scheduler using an existing role, need to make one programatically?
-    const scheduleId = await createSchedule(
-      scheduleName,
-      lambdaArn,
-      scheduleRole
-    );
-
-    console.log("eventbridge schedule created with id: ", scheduleId);
-
-    // TODO: skipping grantInvoke for now since I have a role already. OK? NO?
-    // await grantInvokePermission(lambdaArn, restApiId); // ASK JESSE ABOUT S3 CLEANUP LAMBDA PERMISSIONS
-  } catch (error: unknown) {
-    console.error("Error executing setupWorkflowWebhook: ", error);
-  }
-}
-
-void setupS3CacheBucket();
+export const setupS3CacheBucket = () => {
+  const bucketName = `harrier-${installationHash}-S3`;
+  void createS3(client, bucketName, maxWaitTime);
+};
