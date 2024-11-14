241980c731d6905e67ac3e9dddc84f63e638010d
Wook Kim <h.wook.kim@gmail.com>
2024-11-14 11:01:19 -0800

    fix: change s3 naming convention at setup service
    Co-authored-by: Joel Barton <joelbarton-io@users.noreply.github.com>
    Co-authored-by: Jesse Kercheval <jessekerch@users.noreply.github.com>
    Co-authored-by: Shane Ziegler <shaneziegler@users.noreply.github.com>
    Co-authored-by: Wook Kim <wook2000@users.noreply.github.com>

--- src/services/setupS3CacheBucket.ts
+++ src/services/setupS3CacheBucket.ts
@@ -1,6 +1,6 @@
 // import { config } from "../config/client";
 import { createS3 } from "../utils/aws/s3/createS3";
-import { installationHash } from "../config/installationHash";
+// import { installationHash } from "../config/installationHash";
 import { S3Client } from "@aws-sdk/client-s3";
 import { configHarrier } from "../config/configHarrier";
 
@@ -8,7 +8,7 @@ const client = new S3Client({ region: "us-east-1" });
 const maxWaitTime = 60;
 
 export const setupS3CacheBucket = async () => {
-  const bucketName = `harrier-${installationHash}-s3`;
-  configHarrier.s3Name = bucketName;
+  const bucketName = `${configHarrier.s3Name}`;
+  // configHarrier.s3Name = bucketName;
   await createS3(client, bucketName, maxWaitTime);
 };
