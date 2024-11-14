0406bf35cdfe83b347a25ab0d81e276e835f590e
Jesse Kercheval <56614846+jessekerch@users.noreply.github.com>
2024-11-14 09:50:54 -0800

    Update configHarrier.ts

--- src/config/configHarrier.ts
+++ src/config/configHarrier.ts
@@ -67,4 +67,52 @@ export const harrierLambda_Cleanup = {};
 export const harrierLambda_Scheduler = {};
 export const harrierRestApi = {};
 
+export const workflowPolicyDocument = JSON.stringify({
+    Version: "2012-10-17",
+    Statement: [
+        {
+            Sid: "VisualEditor0",
+            Effect: "Allow",
+            Action: ["ec2:StartInstances", "ec2:StopInstances"],
+            Resource: `arn:aws:ec2:*:${exports.configHarrier.awsAccountId}:instance/*`,
+            Condition: {
+                StringEquals: {
+                    "ec2:ResourceTag/Agent": "Harrier-Runner",
+                },
+            },
+        },
+        {
+            Sid: "VisualEditor1",
+            Effect: "Allow",
+            Action: ["ssm:SendCommand", "logs:CreateLogGroup"],
+            Resource: [
+                `arn:aws:ec2:*:${exports.configHarrier.awsAccountId}:instance/*`,
+                "arn:aws:ssm:*:*:document/AWS-RunShellScript",
+                `arn:aws:logs:*:${exports.configHarrier.awsAccountId}:log-group:*`,
+            ],
+        },
+        {
+            Sid: "VisualEditor2",
+            Effect: "Allow",
+            Action: [
+                "logs:CreateLogStream",
+                "s3:GetBucketTagging",
+                "secretsmanager:GetSecretValue",
+                "logs:PutLogEvents",
+            ],
+            Resource: [
+                "arn:aws:s3:::harrier*",
+                `arn:aws:secretsmanager:*:${exports.configHarrier.awsAccountId}:secret:${exports.configHarrier.secretName}*`,
+                `arn:aws:logs:*:${exports.configHarrier.awsAccountId}:log-group:*:log-stream:*`,
+            ],
+        },
+        {
+            Sid: "VisualEditor3",
+            Effect: "Allow",
+            Action: ["ec2:DescribeInstances", "s3:ListAllMyBuckets"],
+            Resource: "*",
+        },
+    ],
+});
+
 // export const harrierWebhook = {};
