95a0d3e3620f3f938b116d0a42fc2f28f67f5250
joelbarton-io <joelbarton406@gmail.com>
2024-11-14 14:25:37 -0700

    event: build and push
    Co-authored-by: Jesse Kercheval <jessekerch@users.noreply.github.com>
    Co-authored-by: Shane Ziegler <shaneziegler@users.noreply.github.com>
    Co-authored-by: Wook Kim <wook2000@users.noreply.github.com>

--- dist/services/setupApiAndWebhook.js
+++ dist/services/setupApiAndWebhook.js
@@ -12,6 +12,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.setupApiAndWebhook = void 0;
 const createServiceRole_1 = require("../utils/aws/iam/createServiceRole");
 const createAndDeployLambda_1 = require("../utils/aws/lambda/createAndDeployLambda");
+const zipLambda_1 = require("../utils/aws/lambda/zipLambda");
 const setupRestApi_1 = require("../utils/aws/api/setupRestApi");
 const integrateLambdaWithApi_1 = require("../utils/aws/api/integrateLambdaWithApi");
 const deployApi_1 = require("../utils/aws/api/deployApi");
@@ -23,6 +24,7 @@ const stageName = "dev"; // HARDCODED
 function setupApiAndWebhook() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
+            yield (0, zipLambda_1.zipLambda)(lambdaName);
             const serviceRoleArn = yield (0, createServiceRole_1.createServiceRole)(workflowServiceRole, configHarrier_1.workflowPolicyDocument);
             yield (0, createAndDeployLambda_1.createAndDeployLambda)(lambdaName, serviceRoleArn);
             const { restApiId, resourceId } = yield (0, setupRestApi_1.setupRestApi)();
