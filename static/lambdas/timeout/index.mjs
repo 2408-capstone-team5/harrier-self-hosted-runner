484a01892861149b971c112995c2ccf53730ef61
Wook Kim <h.wook.kim@gmail.com>
2024-11-26 05:14:43 -0800

    fix: edit lambdas and respective policies to make delayed shutdown work
    Co-authored-by: jessekerch <jesse.kercheval@gmail.com>
    Co-authored-by: joelbarton-io <joelbarton406@gmail.com>
    Co-authored-by: Shane Ziegler <shanedb@gmail.com>

--- static/lambdas/timeout/index.mjs
+++ static/lambdas/timeout/index.mjs
@@ -1,7 +1,13 @@
 import { EC2Client, StopInstancesCommand } from "@aws-sdk/client-ec2";
-import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
+import {
+  S3Client,
+  GetObjectCommand,
+  PutObjectCommand,
+} from "@aws-sdk/client-s3";
 const REGION = process.env.AWS_REGION;
-const [ec2Client, s3Client] = [EC2Client, S3Client].map(client => new client({ region: REGION }));
+const [ec2Client, s3Client] = [EC2Client, S3Client].map(
+  (client) => new client({ region: REGION })
+);
 
 async function streamToString(stream) {
   const chunks = [];
@@ -11,19 +17,16 @@ async function streamToString(stream) {
   return Buffer.concat(chunks).toString("utf-8");
 }
 
-async function stillIdle(
-    s3BucketName,
-    instanceId
-  ) {
-    const response = await s3Client.send(
-      new GetObjectCommand({
-        Bucket: s3BucketName,
-        Key: `runner-statuses/${instanceId}.json`,
-      })
-    );
-    const bodyString = await streamToString(response.Body);
-    const { status } = JSON.parse(bodyString);
-    return status === 'idle'; 
+async function stillIdle(s3BucketName, instanceId) {
+  const response = await s3Client.send(
+    new GetObjectCommand({
+      Bucket: s3BucketName,
+      Key: `runner-statuses/${instanceId}.json`,
+    })
+  );
+  const bodyString = await streamToString(response.Body);
+  const { status } = JSON.parse(bodyString);
+  return status === "idle";
 }
 
 async function stopInstance(instanceId) {
@@ -36,24 +39,63 @@ async function stopInstance(instanceId) {
   }
 }
 
-export const handler = async ({ instanceId, delayInMinutes, s3BucketName }) => {
-    console.log(
-      `arguments: instanceId: ${instanceId}, delay: ${delayInMinutes}, bucketName: ${s3BucketName}`
+async function updateInstanceStatus(s3BucketName, instanceId, nextStatus) {
+  try {
+    const response = await s3Client.send(
+      new GetObjectCommand({
+        Bucket: s3BucketName,
+        Key: `runner-statuses/${instanceId}.json`,
+      })
     );
-  
-    try {
-      await new Promise(res => setTimeout(res, delayInMinutes * 60 * 1000));
-      const instanceIsStillIdle = await stillIdle(s3BucketName, instanceId)
 
-      if (instanceIsStillIdle) {
-        await stopInstance(instanceId);
-        console.log(`✅ STOPPED instance: ${instanceId} after: ~ ${delayInMinutes} minutes`);
-      } else {
-        console.log(`✅ Instance ${instanceId} is not idle, no action was taken on the instance.`);
-      }
-    } catch (error) {
-      console.error("❌ Error in timeout handler:", error);
-    } 
+    const bodyString = await streamToString(response.Body);
+    const instanceState = JSON.parse(bodyString);
+
+    console.log(
+      `currentStatus: ${instanceState.status}, nextStatus: ${nextStatus}`
+    );
+
+    instanceState.status = nextStatus;
+    await s3Client.send(
+      new PutObjectCommand({
+        Bucket: s3BucketName,
+        Key: `runner-statuses/${instanceId}.json`,
+        Body: JSON.stringify(instanceState),
+        ContentType: "application/json",
+      })
+    );
+
+    console.log(
+      `✅ Status updated to ${nextStatus} for instance ${instanceId}`
+    );
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export const handler = async ({ instanceId, delayInMinutes, s3BucketName }) => {
+  console.log(
+    `arguments: instanceId: ${instanceId}, delay: ${delayInMinutes}, bucketName: ${s3BucketName}`
+  );
+
+  try {
+    await new Promise((res) => setTimeout(res, delayInMinutes * 60 * 1000));
+    const instanceIsStillIdle = await stillIdle(s3BucketName, instanceId);
+
+    if (instanceIsStillIdle) {
+      await stopInstance(instanceId);
+      await updateInstanceStatus(s3BucketName, instanceId, "offline");
+      console.log(
+        `✅ STOPPED instance: ${instanceId} after: ~ ${delayInMinutes} minutes`
+      );
+    } else {
+      console.log(
+        `✅ Instance ${instanceId} is not idle, no action was taken on the instance.`
+      );
+    }
+  } catch (error) {
+    console.error("❌ Error in timeout handler:", error);
+  }
 
   return {
     statusCode: 200,
